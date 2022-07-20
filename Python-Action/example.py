12d57f14aa9514a1b9de30d63d9dfbf0307c5161
ABDURRAHMAN SALIH <92715976+salih-17@users.noreply.github.com>
2022-07-20 20:22:32 +0300

    Create script

--- Python-Action/example.py
+++ Python-Action/example.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from datetime import datetime
+import random
 
 
 
 def main():
-    print (datetime.now())
-    print(pd.__version__)
+    df= pd.DataFrame ({'TimeNow':datetime.now()} , index=[0])
+    file_name = str(int(random.random()*12345)) + "_df.csv"
+    df.to_csv(file_name,index = False)
 
 
 if __name__ == '__main__':
